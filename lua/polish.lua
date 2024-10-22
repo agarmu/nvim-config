2c0a9f6570190e706a457c9be2550e264a3a1f99
Mukul Agarwal <agarmukul23@gmail.com>
2024-10-22 19:12:22 -0400

    add keybindings for scrolling

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,3 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -16,3 +14,12 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+--[[
+    Disable left-right scroll keymaps & replace them with Shift+Up/Down Keymaps
+    due to infrequent need & prevent accidental use.
+--]]
+vim.keymap.set({ "n", "i", "v" }, "<ScrollWheelRight>", "<Nop>")
+vim.keymap.set({ "n", "i", "v" }, "<ScrollWheelLeft>", "<Nop>")
+vim.keymap.set({ "n", "i", "v" }, "<S-ScrollWheelUp>", "<ScrollWheelRight>")
+vim.keymap.set({ "n", "i", "v" }, "<S-ScrollWheelDown>", "<ScrollWheelRight>")
