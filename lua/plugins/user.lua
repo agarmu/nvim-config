6f524f588bf90b719778449c10a8ee3f208a60f7
Mukul Agarwal <agarmukul23@gmail.com>
2024-11-26 17:19:07 -0600

    add snacks plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -106,4 +106,20 @@ return {
   {
     "folke/zen-mode.nvim",
   },
+  -- collection of several plugins related to Quality of Life
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      bufdelete = { enabled = true },
+      dashboard = { enabled = true },
+      git = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
 }
