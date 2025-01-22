8dcd22ee64798493886cfe74d279a1c2302710eb
Mukul Agarwal <agarmukul23@gmail.com>
2025-01-22 16:02:29 -0500

    change colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "catppuccin/nvim" },
+  { "neanias/everforest-nvim" },
+  { "scottmckendry/cyberdream.nvim" },
+  { "maxmx03/fluoromachine.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
